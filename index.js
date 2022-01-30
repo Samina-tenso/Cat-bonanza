4eef48c121a4f63346dcf58012d54dca0902cecf
Samina-tenso <tensammi@gmail.com>
2022-01-30 22:19:13 +0100

    added async

--- index.js
+++ index.js
@@ -1,88 +1,83 @@
 const API_key = "a59ab3f9-0f86-4bfe-bd6e-dcc228fe20eb"
 const baseUrl = "https://api.thecatapi.com/v1/images/search"
 
-
-
-
 const prevButton = document.querySelector("#prev")
 const nextButton = document.querySelector("#next")
+const currentPage = document.querySelector("h2")
+const contentGrid = document.getElementById("content-grid")
+
+
 let page = 0;
 let limit = 12;
 let order = "asc"
 
-const currentPage = document.querySelector("h2")
-currentPage.textContent = `Showing page: ${page}`
-
-function deleteCats() {
-
-    document.getElementById("contentGrid").remove()
-
-}
 
 
+function buttonState(disabled) {
 
-function buttonState() {
-    if (page == 0) {
-        prevButton.disabled = true
-    }
-    else { prevButton.disabled = false }
+    nextButton.disabled = disabled
+    prevButton.disabled = disabled
 
 }
 
 prevButton.addEventListener("click", function () {
     page--
     currentPage.textContent = `Showing page: ${page}`
-
-    deleteCats()
-    getCat()
+    showCats()
 })
 
 
 nextButton.addEventListener("click", function () {
     page++
     currentPage.textContent = `Showing page: ${page}`
-    console.log("i clicked")
-    deleteCats()
-    getCat()
-
-
-
+    showCats()
 })
 
-function getCat() {
+
+async function showCats() {
+
 
     buttonState(true)
-    let url = new URL(baseUrl)
+    const url = new URL(baseUrl)
     url.searchParams.append('page', page)
     url.searchParams.append('limit', limit)
     url.searchParams.append('order', order)
+    currentPage.textContent = `Showing page: ${page}`;
+    contentGrid.textContent = "Loading.."
 
+    try {
+        const response = await fetch(url, { headers: { "x-api-key": API_key, }, });
+        const data = await response.json()
+        getCats(data)
 
-    fetch(url, { headers: { "x-api-key": API_key, } })
-        .then(response => response.json())
-        .then(data => {
-            const contentGrid = document.createElement("div")
+    }
+    catch (err) {
+        contentGrid.textContent = "Something went wrong while fetching data from the server";
 
-            contentGrid.setAttribute("id", "contentGrid")
-            const container = document.getElementById("container")
-
-            for (let i = 0; i < data.length; i++) {
-                const img = document.createElement("img")
-                img.src = data[i].url
-
-                contentGrid.appendChild(img)
-
-
-
-            };
-            container.appendChild(contentGrid)
-        });
+    }
+    finally {
+        buttonState(false)
+        prevButton.disabled = page == 0
+    }
+}
 
 
 
-};
-getCat()
+function getCats(data) {
 
+    contentGrid.textContent = null
+
+    for (let i = 0; i < data.length; i++) {
+        const img = document.createElement("img")
+        img.src = data[i].url
+
+        contentGrid.append(img)
+
+
+    };
+}
+
+showCats()
 
 
 
